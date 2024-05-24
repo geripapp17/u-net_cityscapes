93c6d43237dc2b11c3c3d698b687a01745a5cec8
Gergő Papp <gergo.papp-balint@oxit.hu>
2024-05-24 10:30:48 +0200

    bugfix

--- src/utils/losses.py
+++ src/utils/losses.py
@@ -15,14 +15,16 @@ class DiceLoss(nn.Module):
 
     def _multi_class_dice_coeff(self, logits: torch.Tensor, target: torch.Tensor) -> float:
 
-        num_classes = target.shape[1]
-        true_one_hot = torch.eye(num_classes)[target.squeeze(dim=1)]
+        num_classes = logits.shape[1]
+        target_single_channel = torch.unsqueeze(target, dim=1)
+
+        true_one_hot = torch.eye(num_classes, device=target.device)[target]
         true_one_hot = true_one_hot.permute(0, 3, 1, 2).float()
 
         probs = F.softmax(logits, dim=1)
 
         true_one_hot = true_one_hot.type(logits.type())
-        dims = (0,) + tuple(range(2, target.ndimension()))
+        dims = (0,) + tuple(range(2, target_single_channel.ndimension()))
 
         intersection = torch.sum(probs * true_one_hot, dim=dims)
         cardinality = torch.sum(probs + true_one_hot, dim=dims)
--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -1,9 +1,9 @@
 import torch
 
 
-def pixel_accuracy(preds: torch.Tensor, ys: torch.Tensor) -> float:
+def pixel_accuracy(preds: torch.Tensor, target: torch.Tensor) -> float:
 
     segm = torch.argmax(preds, dim=1)
-    correct = torch.sum(segm == ys)
+    correct = torch.sum(segm == target)
 
-    return correct / torch.numel(ys)
+    return correct / torch.numel(target)
